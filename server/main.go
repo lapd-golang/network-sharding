b9b002f107bd80d916b7791123add7613a7717bf
Lingraj Mahanand <mlingraj@medipedia.io>
2018-12-17 07:07:33 +0530

    change

--- server/main.go
+++ server/main.go
@@ -57,21 +57,22 @@ func (s *Server) PutDSBlock(ctx context.Context, in *ds.NodeDSBlock) (*ds.ProtoD
 //GetDSBlock ...
 func (s *Server) GetDSBlock(ctx context.Context, in *ds.GetDSBlockRequest) (*ds.ProtoDSBlock, error) {
 	blockNum := in.GetBlocknum()
-	log.Printf("Block number enquired is: %v\n", blockNum)
+
 	blocks := []*ds.ProtoDSBlock{}
 
 	blocks = m.MapToProtoBuffer(s.DSBlockChain.DSBlocks, blocks)
-	//var protoB *ds.ProtoDSBlock
+
 	log.Printf("size of blockchain in get is now::: %v\n\n", len(blocks))
 	for _, b := range blocks {
 
 		expectedBlockNum := b.Header.Blocknum
-		log.Printf("here it is -------------expectedBlockNum=%v\n", expectedBlockNum)
+		log.Printf("expectedBlockNum=%v\n", expectedBlockNum)
 
 		if expectedBlockNum == blockNum {
+			log.Printf("blockNum=%v\n", blockNum)
 			return b, nil
 		}
-		log.Printf("here it is -------------blockNum=%v\n", blockNum)
+
 	}
 	return new(ds.ProtoDSBlock), nil
 }
